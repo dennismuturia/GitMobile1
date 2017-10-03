79edade5ce72aa3f9b99623ddddc5e03b5b32d87
dennismuturia <dmuturia2922@gmail.com>
2017-10-03 11:15:30 +0300

    Integrated firebase in the project

---
+++ app/src/main/java/com/example/dennis/gitmobile/Constants.java
@@ -0,0 +1,16 @@
+package com.example.dennis.gitmobile;
+
+/**
+ * Created by dennis on 10/3/17.
+ */
+
+public class Constants {
+    public static final String GITHUB_ACCESS_TOKEN = BuildConfig.githubAPI;
+
+    public static final String GITHUB_CLIENT_ID = BuildConfig.githubClientID;
+    public static final String GITHUB_SECRET_TOKEN = BuildConfig.githubSecretToken;
+    public static final String GITHUB_BASE_URL = "https://api.github.com/user";
+    public static final String GITHUB_ACCESS_TOKEN_NAME = "access_token";
+    public static final String GITHUB_CLIENTID_NAME = "client_id";
+    public static final String GITHUB_CLIENT_SECRET_NAME = "client_secret";
+}
