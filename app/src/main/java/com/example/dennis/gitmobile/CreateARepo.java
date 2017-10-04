ecde352c1a943383c230a19d2be3216fb15db5fd
dennismuturia <dmuturia2922@gmail.com>
2017-10-04 12:48:17 +0300

    Added a create a repo activity that is used to display the text fields to create new repository. Here i will use the post request to pass in the credentials and create a new repository

---
+++ app/src/main/java/com/example/dennis/gitmobile/CreateARepo.java
@@ -0,0 +1,13 @@
+package com.example.dennis.gitmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateARepo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_arepo);
+    }
+}
